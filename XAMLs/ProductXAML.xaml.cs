567756aab36e63e4378a5e82656a6bbc57f7c36a
Aymeric <aymeric.lamboley@gmail.com>
2015-03-10 17:26:22 +0100

    see product category desc

--- XAMLs/ProductXAML.xaml.cs
+++ XAMLs/ProductXAML.xaml.cs
@@ -17,11 +17,24 @@ namespace GeStock {
 			BindingContext = _geStockItemViewModel;
 
 			InitializeComponent ();
+
+			_refresh ();
 		}
 
 		void ShowEditPage(object sender, EventArgs e) {
 
 			Navigation.PushAsync(new EditProductXAML(_geStockItemViewModel));
+		}
+
+		override protected void OnAppearing() {
+			base.OnAppearing();
+
+			_refresh();
+		}
+
+		void _refresh() {
+
+			CategoryDescription.Text = App.Database.GetCategory (_geStockItemViewModel.CategoryIndex).Description;
 		}
 
 	}
